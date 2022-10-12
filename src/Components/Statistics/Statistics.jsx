24f10995c162d65907fe020fd736f68a77f0c989
asaad-tt <aasadshumon86@gmail.comm>
2022-10-12 15:38:46 +0600

    some style add

--- src/Components/Statistics/Statistics.jsx
+++ src/Components/Statistics/Statistics.jsx
@@ -14,26 +14,31 @@ const Statistics = () => {
   const quizzes = useContext(QuizContext);
   // console.log(quizzes);
   return (
-    <div className="flex sm:w-100 justify-center mt-10">
-      <ResponsiveContainer height={500}>
-        <LineChart
-          width={500}
-          height={400}
-          data={quizzes.quizzes.data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <Line type="monotone" dataKey="total" stroke="#82ca9d"></Line>
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip></Tooltip>
-          <CartesianGrid strokeDasharray="3 3" />
-        </LineChart>
-      </ResponsiveContainer>
+    <div>
+      <h1 className="text-center pt-5 font-semibold text-2xl text-purple-500">
+        Graph of Quizzes{" "}
+      </h1>
+      <div className="flex sm:w-100 justify-center my-5">
+        <ResponsiveContainer height={500}>
+          <LineChart
+            width={500}
+            height={400}
+            data={quizzes.quizzes.data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <Line type="monotone" dataKey="total" stroke="#82ca9d"></Line>
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip></Tooltip>
+            <CartesianGrid strokeDasharray="3 3" />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 };
